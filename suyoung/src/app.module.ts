65554dc06e5c16f29179c54be09c79c9d9bab128
soo <sooyoung.choi.dev@gmail.com>
2024-04-18 17:57:56 +0900

    Add: auth 관련 디렉토리 생성

--- suyoung/src/app.module.ts
+++ suyoung/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HttpModule } from '@nestjs/axios';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [HttpModule, UserModule],
+  imports: [HttpModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
