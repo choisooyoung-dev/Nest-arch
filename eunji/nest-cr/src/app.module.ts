5d16b1bb395f889b42d9e246717f4ac583f53545
eunjiUdev <167047856+eunjiUdev@users.noreply.github.com>
2024-04-22 11:44:56 +0900

    Revert "fix: 경로 변경 및 typeorm 을 통한 연결 제거"

--- eunji/nest-cr/src/app.module.ts
+++ eunji/nest-cr/src/app.module.ts
@@ -5,13 +5,32 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MysqlService } from '../config/database.config';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
+const typeOrmModuleOptions = {
+  useFactory: async (
+    configService: ConfigService,
+  ): Promise<TypeOrmModuleOptions> => ({
+    type: 'mysql',
+    username: configService.get<string>('DB_USER'),
+    password: configService.get<string>('DB_PASSWORD'),
+    host: configService.get<string>('DB_HOST'),
+    port: configService.get<number>('DB_PORT'),
+    database: configService.get<string>('DB_NAME'),
+  }),
+  inject: [ConfigService],
+};
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TypeOrmModule.forRootAsync(typeOrmModuleOptions),
+
   ],
   controllers: [AppController],
-  providers: [AppService, MysqlService],
+  providers: [AppService, MysqlService],  
+
 })
-export class AppModule {}
+export class AppModule {} 
+
+
