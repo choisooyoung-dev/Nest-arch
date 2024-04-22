4e44d7343f305b6ae5ce87c497195e1d00cb2352
sooyoung <sooyoung.choi.dev@gmail.com>
2024-04-22 17:32:37 +0900

    Refactor: 환경변수 dotenv -> configservice 변경

--- suyoung/src/app.module.ts
+++ suyoung/src/app.module.ts
@@ -5,10 +5,23 @@ import { HttpModule } from '@nestjs/axios';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { DbConncetionService } from './db/db.conncetion.service';
+import { DbConncetionModule } from './db/db.conncetion.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/db.config';
 
 @Module({
-  imports: [HttpModule, UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+      load: [configuration],
+    }),
+    HttpModule,
+    UserModule,
+    AuthModule,
+    DbConncetionModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, DbConncetionService],
+  providers: [AppService],
 })
 export class AppModule {}
